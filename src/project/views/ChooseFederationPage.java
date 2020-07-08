5926f020cea531895c45d551c6caffedbdea3c3e
mustafa-sevim <mstfsvmbjk@gmail.com>
2020-07-08 23:41:50 +0300

    new

--- src/project/views/ChooseFederationPage.java
+++ src/project/views/ChooseFederationPage.java
@@ -13,11 +13,11 @@ public class ChooseFederationPage extends Page {
 	 @Override
 	public void buildPage() {
 		super.buildPage();
-		int choice=0;
-		for(int i = 0; i < Football.federations.size(); i++) {
+		int choice=0,i=0;
+		for(i = 0; i < Football.federations.size(); i++) {
 			System.out.println((i) + " - " + Football.federations.get(i).getName());
 		}
-		
+		System.out.println((i) + " - Previous Page");
 		do {
 			choice= Integer.parseInt(scnnr.nextLine());
 			choice--;
